f061dcf78edb50d60453999a1878b41bf87cae5d
dead doomed <igna.gallardo@duocuc.cl>
2023-10-26 07:59:00 -0400

    1.3.2

--- src/app/models/UserModel.ts
+++ src/app/models/UserModel.ts
@@ -1,11 +1,16 @@
 export class UserModel {
 
     constructor(
-        public user: string,
-        public name: string,
-        public last_name: string,
-        public rut: number,
-        public pass: string,
+        public numrun: number,
+        public dvrun: number,
+        public first_name: string,
+        public second_name: string,
+        public p_last_name: string,
+        public m_last_name: string,
+        public email: string,
+        public username: string,
+        public id_type: number,
+        public password: string,
         public type: string
     ) {
     }
